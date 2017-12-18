45bace2eb1ba70fffbbe3bb46dfdce231a1b1960
EC2 Default User <ec2-user@ip-172-31-11-5.us-east-2.compute.internal>
2017-12-18 23:54:49 +0000

    edited the flex offset

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
+    render() {
+        return (
+            <div className="App">
         <header className="App-header">
           <Grid fluid>
         <Row>  <Col xsOffset={11} xs={1} />
          <img src="https://i.imgur.com/diJit8O.png" className="App-test" />
           <img src='https://i.pinimg.com/originals/cc/ea/3c/ccea3ceb5d1cc287c84f3c4627679a8e.jpg' className="App-light" />
-
-               <Col xs={6} md={3}>
-             <h1 className="App-title">Welcome</h1>
-            This site is made by a survivor. 
-         
-  <Col xsOffset={10} xs={2} /> demo   </Col>
+               <Col  xsOffset={10}   xs={6} md={3} />
+             <h1 className="App-title">Welcome.
+          </h1>
         </Row>
-        
-      < /Grid>
-                        </header>
+      </Grid>
+     </header>
                   
         <p className="App-intro">
           <p>Welcome to a Resource Site for DV surviors.</p>
@@ -40,10 +35,12 @@ class App extends Component {
                                                 <li><a href="https://www.grants.gov/"> Grants.gov </a></li>
                                                  <img src="https://i.imgur.com/qWHuk4y.jpg" className= "App-quote" />
 
+       <h5>This site is made by a survivor.</h5>
         </p>
       </div>
-    );
-  }
+
+        );
+    }
 }
 
 export default App;
