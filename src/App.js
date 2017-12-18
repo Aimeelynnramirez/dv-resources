31eea2e7726f4c87e7e55908b18a8f71c035283c
EC2 Default User <ec2-user@ip-172-31-11-5.us-east-2.compute.internal>
2017-12-18 23:29:11 +0000

    flex

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 import './App.css';
 
@@ -8,16 +9,27 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <Grid fluid>
+        <Row>  <Col xsOffset={11} xs={1} />
+
                     <img src="https://i.imgur.com/diJit8O.png" className="App-test" />
                     
-          <h1 className="App-title">Welcome</h1>
           <img src='https://i.pinimg.com/originals/cc/ea/3c/ccea3ceb5d1cc287c84f3c4627679a8e.jpg' className="App-light" />
-                  </header>
+
+                  <Col xs={6} md={3}>
+                                    <h1 className="App-title">Welcome</h1>
+
+            This site is made by a survivor. 
+          </Col>
+          
+        </Row>
+      < /Grid>
+                        </header>
                   
         <p className="App-intro">
           <p>Welcome to a Resource Site for DV surviors.</p>
           <code>It is wrong but it's ok. You are ok.</code> 
-          <p>Take a stand.</p>
+          <p>Take a stand.</p>         
 
           <h1><a href="http://webserver.rilin.state.ri.us/Statutes/TITLE15/INDEX.HTM">RI State Law</a></h1>
           <li> <a href="http://webserver.rilin.state.ri.us/Statutes/TITLE15/15-15/INDEX.HTM">Index of Sections</a></li>
@@ -28,7 +40,7 @@ class App extends Component {
                                                 <li><a href="https://www.grants.gov/"> Grants.gov </a></li>
                                                 <li><a href="https://capitalgoodfund.org/en/"> CaptialGoodFund </a></li>
                                                 <li><a href="https://www.grants.gov/"> Grants.gov </a></li>
-
+                                                 <img src="https://i.imgur.com/qWHuk4y.jpg" className= "App-quote" />
 
         </p>
       </div>
