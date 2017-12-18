7c7b3cbf29f3bb49ba1cc7ba6cd5f16314240bc7
EC2 Default User <ec2-user@ip-172-31-11-5.us-east-2.compute.internal>
2017-12-18 23:10:21 +0000

    lesson

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
@@ -7,11 +8,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-                    <img src='https://ncadv.org/assets/2497/takeastand_v05.png' className="App-test" />
+                    <img src="https://i.imgur.com/diJit8O.png" className="App-test" />
                     
           <h1 className="App-title">Welcome</h1>
           <img src='https://i.pinimg.com/originals/cc/ea/3c/ccea3ceb5d1cc287c84f3c4627679a8e.jpg' className="App-light" />
                   </header>
+                  
         <p className="App-intro">
           <p>Welcome to a Resource Site for DV surviors.</p>
           <code>It is wrong but it's ok. You are ok.</code> 
@@ -21,10 +23,11 @@ class App extends Component {
           <li> <a href="http://webserver.rilin.state.ri.us/Statutes/TITLE15/15-15/INDEX.HTM">Index of Sections</a></li>
                     <li> <a href="http://webserver.rilin.state.ri.us/Statutes/TITLE15/15-15/15-15-5.HTM">Domestic Abuse Prevention</a></li>
             <h2>Get Help:</h2>
-                        <h1><a href="http://www.sojournerri.org"> SojournerRI </a></h1>
-                                                <h1><a href="https://www.grants.gov/"> Grants.gov </a></h1>
-                                                <h1><a href="https://capitalgoodfund.org/en/"> CaptialGoodFund </a></h1>
-                                                <h1><a href="https://www.grants.gov/"> Grants.gov </a></h1>
+                        <li><a href="http://www.sojournerri.org"> SojournerRI </a></li>
+                        <li><a href="https://www.helprilaw.org"> Rhode Island Legal Services </a></li>
+                                                <li><a href="https://www.grants.gov/"> Grants.gov </a></li>
+                                                <li><a href="https://capitalgoodfund.org/en/"> CaptialGoodFund </a></li>
+                                                <li><a href="https://www.grants.gov/"> Grants.gov </a></li>
 
 
         </p>
