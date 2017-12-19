eec9b1518cb241ad3c12d4574cb8958ba898eb08
EC2 Default User <ec2-user@ip-172-31-46-155.us-east-2.compute.internal>
2017-12-19 11:13:24 +0000

    sorted

--- src/App.js
+++ src/App.js
@@ -2,50 +2,67 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import './App.css';
-
 class App extends Component {
     render() {
         return (
             <div className="App">
+                    <h1 className="App-title">Welcome.</h1>
+
         <header className="App-header">
-          <Grid fluid>
-          
-        <Row> <h1 className="App-title">Welcome.</h1>
- <Col xsOffset={11} xs={1} />
-
-         <img src="https://i.imgur.com/diJit8O.png" className="App-test" />
- <Col xsOffset={10} xs={1} />
-
-                  <img src='https://i.pinimg.com/originals/cc/ea/3c/ccea3ceb5d1cc287c84f3c4627679a8e.jpg' className="App-light" />
-
-
-        </Row>
+        
+    
+        <Grid fluid>
+        <Row center="xs">
+      <Col xs={6} />  
+<p>
+         <img src="https://i.imgur.com/diJit8O.png" className="App-test" /> "Leslie Morgan Steiner was in "crazy love" -- that is, madly in love with a man who routinely abused her and threatened her life. Steiner tells the story of her relationship, correcting misconceptions many people hold about victims of domestic violence, and explaining how we can all help break the silence."
+   <Col xs={6} />  <p>Watch the video here:< a href="https://www.ted.com/talks/leslie_morgan_steiner_why_domestic_violence_victims_don_t_leave?utm_campaign=tedspread--b&utm_medium=referral&utm_source=tedcomshare">TEDtalks</a> Ideas Worth Spreading</p>
+       </p>   </Row>
       </Grid>
      </header>
-
+ 
         <p className="App-intro">
-          <p>Welcome to a Resource Site for DV survivors.</p>
-          <code>It is wrong but it's ok. You are ok.</code>
-          <p>Take a stand.</p>
-                 <img src="https://i.imgur.com/UdErcrT.png" className="App-chat"/>
+                <img src="https://i.imgur.com/qWHuk4y.jpg" className="App-quote"/>                                       
 
+        <Grid fluid>
+          
+     <Row start="xs">
+      <Col xs={6} />
+
+               <p className="App-intro">
+   <p>Welcome to a Resource Site for DV survivors.</p>
+           <code>It is wrong but it's ok. You are ok, if not you will be.</code>
+      <Col xs={6} />
+
+         <p>Take a stand.</p>
+         <a href="http://www.thehotline.org"><img src="https://i.imgur.com/UdErcrT.png" title="source: thehotline.org" className="App-quote" /></a>
+         
 
           <h1><a href="http://webserver.ripn.state.ri.us/Statutes/TITLE15/INDEX.HTM">RI State Law</a></h1>
           <p> <a href="http://webserver.ripn.state.ri.us/Statutes/TITLE15/15-15/INDEX.HTM">Index of Sections</a></p>
-                    <p> <a href="http://webserver.ripn.state.ri.us/Statutes/TITLE15/15-15/15-15-5.HTM">Domestic Abuse Prevention</a></p>
+          <p> <a href="http://webserver.ripn.state.ri.us/Statutes/TITLE15/15-15/15-15-5.HTM">Domestic Abuse Prevention</a></p>
+  </p>
+          <p className="App-two">
+
+  <Col xs />
+
           <h2>Get Help:</h2>
-                        <p><a href="http://www.sojournerri.org"> SojournerRI </a></p>
+
+                        <p> <a href="http://www.sojournerri.org"> SojournerRI </a></p>
+                        <p><a href=" https://www.helpguide.org/"> HelpGuide.org </a></p>
                                                 <p><a href="http://www.thehotline.org/"> National Hotline </a></p>
                                                 <p><a href="https://www.helprilaw.org"> Rhode Island Legal Services </a></p>
                                                 <p><a href="https://www.grants.gov/"> Grants.gov </a></p>                                               
-                                                <p><a href="http://www.211.org/"> 211 </a></p>
                                                 <p><a href="https://capitalgoodfund.org/en/"> CapitalGoodFund </a></p>
-                                                <p><a href="https://www.grants.gov/"> Grants.gov </a></p>
+                                                 <p><a href="http://www.211.org/"> 211 </a></p>
                                                 <p><a href="http://www.dlt.ri.gov/aboutdlt.htm"> RI Department Labor and Training</a></p>
-
-                                                 <img src="https://i.imgur.com/qWHuk4y.jpg" className= "App-quote" />
+ </p></Row>
+      </Grid>
+                                                
          <h5>This site is made by a survivor. </h5>
-<img src="https://i.imgur.com/SKjfaOX.png" className="App-profile"/>        </p>
+<img src="https://i.imgur.com/SKjfaOX.png?1" className="App-profile"/> 
+
+      </p>
       </div>
 
         );
