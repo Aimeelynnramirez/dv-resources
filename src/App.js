21d243e9e41968318a40a12ce80acf804e6cb15c
EC2 Default User <ec2-user@ip-172-31-16-108.us-east-2.compute.internal>
2017-12-19 04:08:20 +0000

    typo

--- src/App.js
+++ src/App.js
@@ -15,27 +15,28 @@ class App extends Component {
  <Col xsOffset={11} xs={1} />
 
          <img src="https://i.imgur.com/diJit8O.png" className="App-test" />
-         <img src='https://i.pinimg.com/originals/cc/ea/3c/ccea3ceb5d1cc287c84f3c4627679a8e.jpg' className="App-light" />
+                  <img src='https://i.pinimg.com/originals/cc/ea/3c/ccea3ceb5d1cc287c84f3c4627679a8e.jpg' className="App-light" />
+
         </Row>
       </Grid>
      </header>
 
         <p className="App-intro">
-          <p>Welcome to a Resource Site for DV surviors.</p>
+          <p>Welcome to a Resource Site for DV survivor.</p>
           <code>It is wrong but it's ok. You are ok.</code>
           <p>Take a stand.</p>
                  <img src="https://i.imgur.com/UdErcrT.png" className="App-chat"/>
 
 
-          <h1><a href="http://webserver.rilin.state.ri.us/Statutes/TITLE15/INDEX.HTM">RI State Law</a></h1>
-          <li> <a href="http://webserver.rilin.state.ri.us/Statutes/TITLE15/15-15/INDEX.HTM">Index of Sections</a></li>
-                    <li> <a href="http://webserver.rilin.state.ri.us/Statutes/TITLE15/15-15/15-15-5.HTM">Domestic Abuse Prevention</a></li>
+          <h1><a href="http://webserver.ripn.state.ri.us/Statutes/TITLE15/INDEX.HTM">RI State Law</a></h1>
+          <p> <a href="http://webserver.ripn.state.ri.us/Statutes/TITLE15/15-15/INDEX.HTM">Index of Sections</a></p>
+                    <p> <a href="http://webserver.ripn.state.ri.us/Statutes/TITLE15/15-15/15-15-5.HTM">Domestic Abuse Prevention</a></p>
             <h2>Get Help:</h2>
-                        <li><a href="http://www.sojournerri.org"> SojournerRI </a></li>
-                        <li><a href="https://www.helprilaw.org"> Rhode Island Legal Services </a></li>
-                                                <li><a href="https://www.grants.gov/"> Grants.gov </a></li>
-                                                <li><a href="https://capitalgoodfund.org/en/"> CaptialGoodFund </a></li>
-                                                <li><a href="https://www.grants.gov/"> Grants.gov </a></li>
+                        <p><a href="http://www.sojournerri.org"> SojournerRI </a></p>
+                        <p><a href="https://www.helprilaw.org"> Rhode Island Legal Services </a></p>
+                                                <p><a href="https://www.grants.gov/"> Grants.gov </a></p>
+                                                <p><a href="https://capitalgoodfund.org/en/"> CaptialGoodFund </a></p>
+                                                <p><a href="https://www.grants.gov/"> Grants.gov </a></p>
                                                  <img src="https://i.imgur.com/qWHuk4y.jpg" className= "App-quote" />
 
        <h5>This site is made by a survivor. </h5>
